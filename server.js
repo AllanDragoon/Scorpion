1e07641ce82564f20a3fa1847b0a6bac0f1c9847
AllanDragoon <AllanDragoon@gmail.com>
2017-01-10 17:14:44 +0800

    Add server.js to start webpack dev server and update package.json

---
+++ server.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack');
+var WebPackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.dev');
+
+var server = new WebPackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    progress: true,
+    state:{
+        colors: true
+    }
+});
+
+servevr.app.get('*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8089, function(){
+    console.log('正常打开8089端口');
+});
