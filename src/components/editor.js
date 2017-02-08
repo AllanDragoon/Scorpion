f3be74aa083a53358abd03c5618c93c3047a722e
AllanDragoon <AllanDragoon@gmail.com>
2017-02-08 13:49:45 +0800

    Editor's zoom and pan by d3.

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import * as d3 from 'd3';
 import Grid from './grid';
 
 class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.zoomed = this.zoomed.bind(this);
+    }
+
+    componentDidMount() {
+        var bindZoom = d3.zoom()
+            .scaleExtent([1, 10])
+            .on("zoom", this.zoomed);
+        var svgNode = this.refs.svg;
+        d3.select(svgNode).call(bindZoom);
+    }
+
+    zoomed() {
+        var containerNode = this.refs.container;
+        var transform = d3.event.transform;
+        d3.select(containerNode).
+            attr("transform", 
+                "translate(" + transform.x + "," + transform.y + ")scale(" + transform.k + ")");
+    }
+
     render() {
         return (
-            <svg width='100%' height='100%'>
-                <Grid />
+            <svg width='100%' height='100%' ref='svg'>
+                <g ref='container'>
+                    <Grid />
+                </g>
             </svg>
         );
     }
