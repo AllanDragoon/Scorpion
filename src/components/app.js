771052e6b5ab9f43087bf29ca37951839b76789b
AllanDragoon <AllanDragoon@gmail.com>
2017-02-10 16:01:30 +0800

    Add arrow buttons.

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,8 @@ import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import IconButton from 'material-ui/IconButton';
+import ArrowIcon from 'material-ui/svg-icons/maps/navigation';
 
 import Header from './header';
 import Editor from './editor';
@@ -29,6 +31,16 @@ class App extends Component {
     }
 
     render() {
+        const iconStyle = { 
+            width: 48,
+            height: 48
+        };
+        const buttonStyle = {
+            width: 48,
+            height: 48,
+            padding: 0
+        };
+
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
                 <div className="app">
@@ -38,7 +50,29 @@ class App extends Component {
                             <Editor ref="editor" width={this.state.width} height={this.state.height} />
                         </div>
                     </Measure>
-                </div>
+                    <div className="drawBtnContainer">
+                        <div className="btn up">
+                            <IconButton style={buttonStyle} iconStyle={iconStyle}>
+                                <ArrowIcon />
+                            </IconButton>
+                        </div>
+                        <div className="btn down">
+                            <IconButton style={buttonStyle} iconStyle={iconStyle}>
+                                <ArrowIcon />
+                            </IconButton>
+                        </div>
+                        <div className="btn left">
+                            <IconButton style={buttonStyle} iconStyle={iconStyle}>
+                                <ArrowIcon />
+                            </IconButton>
+                        </div>
+                        <div className="btn right">
+                            <IconButton style={buttonStyle} iconStyle={iconStyle}>
+                                <ArrowIcon />
+                            </IconButton>
+                        </div>
+                    </div>
+                </div>                
             </MuiThemeProvider>
         );
     }
